506c382a5a8f04cf0d93d5545fc16f186853c3b6
1Lucxxy <dafaraza61@gmail.com>
2025-12-22 22:09:17 -0800

    Update aimbot.lua

--- Script-Games/main/aimbot.lua
+++ Script-Games/main/aimbot.lua
@@ -1,22 +1,8 @@
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
+local success, OrionLib = pcall(function()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
+end)
 
-local Window = OrionLib:MakeWindow({
-    Name = "TEST GUI",
-    HidePremium = false,
-    SaveConfig = false
-})
-
-local Tab = Window:MakeTab({
-    Name = "Test",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
-
-Tab:AddButton({
-    Name = "GUI KEDETEKSI",
-    Callback = function()
-        print("GUI WORK")
-    end
-})
-
-OrionLib:Init()
+if not success then
+    warn("ORION GAGAL LOAD")
+    return
+end
